d56e92818f595ca623c7c41ac0f02e52ab22e037
Da3az <c-a-b@live.fr>
2020-10-29 22:35:10 +0000

    better

---
+++ public/precache-manifest.b5e03c1087c1a1e131a1a130ad984659.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d76969f01e525d3933db8eedda0464",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6a08b40511dbaf449fd",
+    "url": "/static/css/main.8683242f.chunk.css"
+  },
+  {
+    "revision": "f4beec0ea9ae4f79d844",
+    "url": "/static/js/2.ad3e5793.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ad3e5793.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6a08b40511dbaf449fd",
+    "url": "/static/js/main.64469f7b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose')
+const port = process.env.PORT||5000;
+let Score = require('./score.model.js')
+require('dotenv').config();
+// const uri = process.env.ATLAS_URI
+const uri = process.env.ATLAS_URI
+mongoose.connect(uri,{ useNewUrlParser: true,useCreateIndex: true,useUnifiedTopology: true});
+const connection = mongoose.connection
+connection.once('open',()=>console.log('mongodb database'))
+
+
+app.use(cors());
+app.use(express.json());
+
+// const query =  ;
+const options = {
+  sort: {score:1},
+  projection: {
+    _id:0,
+    score:1,
+    username:1
+  }
+};
+
+app.get('/',(req,res)=>{
+  console.log(req.body)
+  Score.find().sort({score:-1})
+              .limit(10)
+                   .then(scores => res.json(scores))
+                   .catch(err => res.status(400).json('Error: ' + err))
+})
+
+app.post('/',(req,res)=>{
+  console.log('request message >>>>>>>>',req.body);
+  const username = req.body.user;
+  const score = req.body.score;
+  console.log('data::::::',username,score);
+  const newScore = new Score({username,score})
+  newScore.save()
+          .then(() => res.json('New score'))
+          .catch(err => res.status(400).json('Error: ' + err))
+})
+
+
+app.listen(port,() => console.log('Server is running'))
